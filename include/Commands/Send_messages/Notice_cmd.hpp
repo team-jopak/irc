928d77fb718d78a0997da1112ab2c2ca996bb3f5
ajy-dev <114038927+ajy-dev@users.noreply.github.com>
2023-02-12 16:38:39 +0900

    Add space between receiver and message

--- include/Commands/Send_messages/Notice_cmd.hpp
+++ include/Commands/Send_messages/Notice_cmd.hpp
@@ -84,7 +84,7 @@ public:
                         {
                             if (!(*it_ch)->is_talkable(client))
                                 throw Err_404((*it_ch)->get_name());
-                            (*it_ch)->message_channel(client->get_message_prefix() + " NOTICE " + (*it) + _message);
+                            (*it_ch)->message_channel(client->get_message_prefix() + " NOTICE " + (*it) + " "  + _message);
                         }
                     }
                 }
@@ -95,7 +95,7 @@ public:
                         throw Err_401(*it);
                     if (!dest->is_talkable(client))
                         throw Err_404(dest->get_name());
-                    dest->message_channel(client->get_message_prefix() + " NOTICE " + (*it) + _message);
+                    dest->message_channel(client->get_message_prefix() + " NOTICE " + (*it) + " " + _message);
                 }
             }
             else
@@ -106,7 +106,7 @@ public:
                     for (Server::list_client::iterator it_cl = cl_list.begin(); it_cl != cl_list.end(); it_cl++)
                     {
                         if (ft::strmatch((*it), (*it_cl)->get_nickname()))
-                            (*it_cl)->message_client(client->get_message_prefix() + " NOTICE " + (*it) + _message);
+                            (*it_cl)->message_client(client->get_message_prefix() + " NOTICE " + (*it) + " " + _message);
                     }
                 }
                 else
@@ -114,7 +114,7 @@ public:
                     Client *dest = server->get_client_by_nickname(*it);
                     if (!dest)
                         throw Err_401(*it);
-                    dest->message_client(client->get_message_prefix() + " NOTICE " + (*it) + _message);
+                    dest->message_client(client->get_message_prefix() + " NOTICE " + (*it) + " " + _message);
                 }
             }
         }
