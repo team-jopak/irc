50fca6516721992a5ff82ee67849e6fe8d246ce0
Jeongyoon An <jeoan@c4r8s4.42seoul.kr>
2023-02-16 16:12:56 +0900

    Fix privmsg/notice

--- include/Commands/Send_messages/Privmsg_cmd.hpp
+++ include/Commands/Send_messages/Privmsg_cmd.hpp
@@ -94,7 +94,7 @@ public:
                         {
                             if (!(*it_ch)->is_talkable(client))
                                 throw Err_404((*it_ch)->get_name());
-                            (*it_ch)->message_channel(client->get_message_prefix() + " PRIVMSG " + (*it) + " " + _message);
+                            (*it_ch)->message_channel_except_sender(":" + client->get_message_prefix() + " PRIVMSG " + (*it) + " " + _message, client);
                         }
                     }
                 }
@@ -105,7 +105,7 @@ public:
                         throw Err_401(*it, false);
                     if (!dest->is_talkable(client))
                         throw Err_404(dest->get_name());
-                    dest->message_channel(client->get_message_prefix() + " PRIVMSG " + (*it) + " " + _message);
+                    dest->message_channel_except_sender(":" + client->get_message_prefix() + " PRIVMSG " + (*it) + " " + _message, client);
                 }
             }
             else
@@ -116,7 +116,7 @@ public:
                     for (Server::list_client::iterator it_cl = cl_list.begin(); it_cl != cl_list.end(); it_cl++)
                     {
                         if (ft::strmatch((*it), (*it_cl)->get_nickname()))
-                            (*it_cl)->message_client(client->get_message_prefix() + " PRIVMSG " + " " + (*it) + _message);
+                            (*it_cl)->message_client(":" + client->get_message_prefix() + " PRIVMSG " + " " + (*it) + _message);
                     }
                 }
                 else
@@ -124,7 +124,7 @@ public:
                     Client *dest = server->get_client_by_nickname(*it);
                     if (!dest)
                         throw Err_401(*it, true);
-                    dest->message_client(client->get_message_prefix() + " PRIVMSG " + " " + (*it) + _message);
+                    dest->message_client(":" + client->get_message_prefix() + " PRIVMSG " + " " + (*it) + _message);
                 }
             }
         }
