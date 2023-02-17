7e139b9711ed3cdea01095a6b110d500dee65965
Jeongyoon An <jeoan@c4r1s6.42seoul.kr>
2023-02-17 17:35:03 +0900

    Add basic kick command

--- include/Commands/Channel/Kick_cmd.hpp
+++ include/Commands/Channel/Kick_cmd.hpp
@@ -4,4 +4,63 @@
 #include "../Command.hpp"
 #include "../../util_funcs.hpp"
 
+class Kick_cmd : public Command
+{
+private:
+    std::string channel_strs;
+    std::string user_strs;
+    std::string message;
+
+public:
+    Kick_cmd() : Command("KICK")
+    {
+    }
+
+    virtual void parse_args(list_str args)
+    {
+        list_str_iter   it = args.begin();
+
+        channel_strs = *it++;
+        user_strs = *it++;
+        message = *it;
+    }
+
+    // op, joined, invited, voice에서 제거
+    virtual void execute(Server* server, Client* client)
+    {
+        Channel*        ch;
+        Client*         user;
+
+        user = server->get_client_by_nickname(user_strs);
+        if (channel_strs.size() == 0 || user_strs.size() == 0)
+        {
+            // ERR_NEEDMOREPARAMS
+        }
+
+        ch = server->get_channel(channel_strs);
+        if (ch == NULL)
+        {
+            // ERR_NOSUCHCHANNEL
+        }
+
+        if (!ch->joined->exist(user))
+        {
+            // ERR_NOTONCHANNEL
+        }
+
+        ch->op->del(user);
+        ch->joined->del(user);
+        ch->invited->del(user);
+        ch->voice->del(user);
+        user->delete_channel(ch);
+        init_cmd();
+    }
+
+    virtual void init_cmd()
+    {
+        std::cout << "Init command" << std::endl;
+    }
+
+};
+
 #endif
