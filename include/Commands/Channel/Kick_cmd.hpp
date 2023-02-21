0976f35210a35462ada1364e665aecfefb8b9947
Jeongyoon An <jeoan@c4r1s7.42seoul.kr>
2023-02-21 15:46:15 +0900

    Fix message with space

--- include/Commands/Channel/Kick_cmd.hpp
+++ include/Commands/Channel/Kick_cmd.hpp
@@ -14,6 +14,9 @@ private:
 public:
     Kick_cmd() : Command("KICK")
     {
+        channel_strs.clear();
+        user_strs.clear();
+        message.clear();
     }
 
     virtual void parse_args(list_str args)
@@ -26,10 +29,14 @@ public:
         }
         channel_strs = *it++;
         user_strs = *it++;
-        if (args.size() == 3)
-            message = *it;
+        if (args.size() >= 3)
+        {
+            for (; it != args.end(); it++)
+                message += (*it) + " ";
+            message.pop_back();
+        }
         else
-            message = "";
+            message.clear();
     }
 
     // op, joined, invited, voice에서 제거
@@ -64,6 +71,9 @@ public:
 
     virtual void init_cmd()
     {
+        channel_strs.clear();
+        user_strs.clear();
+        message.clear();
     }
 
 };
